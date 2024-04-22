8d197564b4801d8781d2b9af9302ad494b27cd3c
brian boudrioux <brian.boudrioux@wildcodeschool.com>
2024-04-22 14:25:25 +0200

    setup workshop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import '../assets/css/navbar.css';
 
 function Navbar() {
+
+  const navigate = useNavigate();
+
   return (
-    <nav className='dark'>
-      <h1>React context</h1>
+    <nav className="dark">
+      <h1 onClick={() => navigate("/")}>React context</h1>
       <label id="switch" className="switch">
-          <input type="checkbox" id="slider" />
-          <span className="slider round"></span>
+        <input type="checkbox" id="slider" />
+        <span className="slider round"></span>
       </label>
     </nav>
   );
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,9 @@ import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 
 function Product({data}) {
+
   const navigate = useNavigate();
+  
   return (
     <article onClick={() => navigate(`/products/${data.id}`)}>
       <h3>{data.title}</h3>
